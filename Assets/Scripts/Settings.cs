c755e4da0f66c831f933ac0874e5ef747d81cad7
restored460 <120643033+restored460@users.noreply.github.com>
2023-03-30 12:33:12 +0100

    UI working

---
+++ Assets/Scripts/Settings.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class Settings : MonoBehaviour
+{
+    public void StartGame()
+    {
+        SceneManager.LoadScene("StartMenu");     
+    }
+    public void Exit()
+    {
+        Application.Quit();
+    }
+    public AudioMixer audioMixer;
+    public void SetVolume(float volume)
+    {
+        audioMixer.SetFloat("MasterVolume", volume);
+    }
+}
